00ce43a994abc4829c688651b7fc54f4be87374e
raihanr1 <raihanrobbani123@gmail.com>
2021-12-07 17:05:53 +0700

    ongoing to deploy docker and heroku

--- server-side/services/users/app.js
+++ server-side/services/users/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 4001;
+const port = process.env.PORT || 4001;
 const { connectMongoDB } = require("./mongodb");
 const Controller = require("./controllers/ControllerUser");
 app.use(cors());
