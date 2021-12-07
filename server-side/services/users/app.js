d343b457f71175221da96cbf64f8587b8222a7fa
raihanr1 <raihanrobbani123@gmail.com>
2021-12-08 00:01:29 +0700

    deploy

--- server-side/services/users/app.js
+++ server-side/services/users/app.js
@@ -4,6 +4,11 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4001;
 const { connectMongoDB } = require("./mongodb");
+connectMongoDB().then((data) => {
+  app.listen(port, () => {
+    console.log(`Listening on port http://localhost:${port}/mongo`);
+  });
+});
 const Controller = require("./controllers/ControllerUser");
 app.use(cors());
 app.use(express.json());
@@ -18,8 +23,6 @@ app.delete("/users/:id", Controller.deleteUsers);
 
 app.put("/users/:id", Controller.updateUsers);
 
-connectMongoDB().then((data) => {
-  app.listen(port, () => {
-    console.log(`Listening on port http://localhost:${port}/mongo`);
-  });
+app.listen(port, () => {
+  console.log(`Listening on port http://localhost:${port}/mongo`);
 });
